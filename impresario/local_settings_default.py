696a255b7496948bc0102a1eb9092e935b7b720d
Piotr Dobrysiak <sip.nestor@gmail.com>
2017-02-07 23:37:47 +0100

    First settings.

---
+++ impresario/local_settings_default.py
@@ -0,0 +1,7 @@
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# Sample python code:
+# from django.utils.crypto import get_random_string
+# chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+# get_random_string(50, chars)
+SECRET_KEY = 'n$44_x+23^epe4%6+f)gch=#&x)5k0obt_e_hpp3s651s5dzsq'
